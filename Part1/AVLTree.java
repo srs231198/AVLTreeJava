5de209df10f6216c2bbf7e61ca6560e182248d46
Shreyas Raj Singh <sxs170100@utdallas.edu>
2020-02-28 20:57:02 -0600

    coded the right rotate function

--- Part1/AVLTree.java
+++ Part1/AVLTree.java
@@ -6,6 +6,7 @@ class AVLTree {
 
     //helper function to get the height of the avl tree
     public int getHeight(AVLNode node) {
+
         //if node is null then return -1
         if(node == null){
             return -1;
@@ -16,9 +17,10 @@ class AVLTree {
 
     //helper function to get the balance of the tree or sub-tree
     public int getBalance(AVLNode node) {
+
         //if node is null return -1
         if(node == null){
-            return -1
+            return -1;
         }
         //Otherwise return the height of left subtree minus the height 
         //of the right subtree
@@ -30,7 +32,25 @@ class AVLTree {
         return (a > b) ? a : b;
     }
 
-    
+    //function to rotate the tree right
+    public AVLNode rightRotate(AVLNode grandParent) {
+
+        //Set the Parent and child nodes
+        AVLNode Parent = grandParent.leftPtr;
+        AVLNode child  = Parent.rightPtr;
+
+        //Swap the nodes and rotate the tree right
+        grandParent.leftPtr = child;
+        Parent.rightPtr = grandParent;
+
+        //Update the tree heights
+        grandParent.height = max(getHeight(grandParent.leftPtr), getHeight(grandParent.rightPtr)) + 1;
+        Parent.height = max(getHeight(Parent.leftPtr), getHeight(Parent.rightPtr)) + 1;
+
+        //return the new root
+        return Parent;
+    }
+
 
 
 
